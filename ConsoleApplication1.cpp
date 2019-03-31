12ab5ef481dab81fd087791a4f1b6b0503f76e88
VitasVital <48451734+VitasVital@users.noreply.github.com>
2019-03-31 13:52:40 +0300

    Amirdobriyvecher

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+struct Node
+{
+	int data;
+	Node* next;
+	Node* getNext()
+	{
+		return next;
+	}
+	void setNext(Node* node)
+	{
+		node->next = next;
+	}
+
+};
+
+class LinkedList
+{
+private:
+	Node* start = NULL;
+	int length = 0;
+
+public:
+
+
+	Node* getStart()
+	{
+		return start;
+	}
+	void addLinkedLIst(int data)
+	{
+		Node*temp = new Node();
+		temp->next = start;
+		temp->data = data;
+		start = temp;
+	}
+
+	void printThemALL()
+	{
+		for (Node* start = getStart(); start != NULL; start = start->getNext())
+		{
+			cout << start->data;
+		}
+	}
+
+
+};
+
+
+
+int main()
+{
+	LinkedList l;
+	l.addLinkedLIst(1);
+	l.addLinkedLIst(3);
+	l.printThemALL();
+
+}
+/*
+1. создаем объект класса, записываем в файл, считываем из файла
+*/
